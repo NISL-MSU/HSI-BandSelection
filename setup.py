cd458432ce3c8daf630102189493ea3eeaeac3a4
Giorgio Morales Luna <giorgiomoralesluna@gmail.com>
2024-03-27 09:14:29 -0600

    Packaged data

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ setuptools.setup(
     url='https://github.com/NISL-MSU/HSI-BandSelection',
     project_urls={"Bug Tracker": "https://github.com/NISL-MSU/HSI-BandSelection/issues"},
     license='MIT',
-    # packages=setuptools.find_packages('src', exclude=['test']),
-    packages=setuptools.find_namespace_packages(where="src"),
+    packages=setuptools.find_packages('src', exclude=['test']),
+    # packages=setuptools.find_namespace_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=['matplotlib', 'numpy', 'opencv-python', 'statsmodels', 'tqdm', 'h5py', 'pyodbc', 'regex',
                       'torchsummary', 'python-dotenv', 'omegaconf', 'pandas', 'importlib-resources'],
-    package_data={'HSIBandSelection.Data': ['*.mat']}
+    # package_data={'HSIBandSelection.Data': ['*.mat']}
 )
